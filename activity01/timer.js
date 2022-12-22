681f97d23230886df007ef6839cdde5014fdb718
Alejandro Antelo <alfasant@protonmail.com>
2022-12-22 20:22:50 +0100

    Added activity01

---
+++ activity01/timer.js
@@ -0,0 +1,15 @@
+export function timer(time) {
+  const promise = new Promise((onresolve, onreject) => {
+    var tmp = setTimeout(() => {
+      onresolve('Time elapsed')
+      clearTimeout(tmp)
+    }, time)
+
+    var wtf = setTimeout(() => {
+      onreject("Time's glitched, it must've been written in JavaScript")
+      clearTimeout(wtf)
+    }, time*2)
+  })
+
+  return promise
+}
