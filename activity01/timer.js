ae2ca3dc35d1080f8c645ff6d305ab0172a1fa28
Alejandro Antelo <alfasant@protonmail.com>
2022-12-22 22:02:17 +0100

    Added comments

--- activity01/timer.js
+++ activity01/timer.js
@@ -1,10 +1,13 @@
 export function timer(time) {
   const promise = new Promise((onresolve, onreject) => {
+    // Create a timer that resolves the promise once the specified time passes
     var tmp = setTimeout(() => {
       onresolve('Time elapsed: '+(time/1000)+'s')
       clearTimeout(tmp)
     }, time)
 
+    // This timer won't finish before the last one,
+    // but if it did, it the promise would be rejected
     var wtf = setTimeout(() => {
       onreject("Time's glitched, it must've been written in JavaScript")
       clearTimeout(wtf)
--- activity02/timer.js
+++ activity02/timer.js
@@ -6,17 +6,21 @@
 function timer(time, element, output = 'Time elapsed: '+(time/1000)+'s') {
   element.textContent = 'Loading...'
   const promise = new Promise((onresolve, onreject) => {
+    // Create a timer that resolves the promise once the specified time passes
     var tmp = setTimeout(() => {
       onresolve(output)
       clearTimeout(tmp)
     }, time)
 
+    // This timer won't finish before the last one,
+    // but if it did, it the promise would be rejected
     var wtf = setTimeout(() => {
       onreject("Time's glitched, it must've been written in JavaScript")
       clearTimeout(wtf)
     }, time*2)
   })
 
+  // Change the contents of the h1 element when the timer finishes
   const handler = result => { element.textContent = result }
   promise
     .then(handler)
@@ -31,10 +35,13 @@ function timer(time, element, output = 'Time elapsed: '+(time/1000)+'s') {
  */
 function countdown(iterations, element, callbackfn, interval = 1000) {
   const promise = new Promise((onresolve) => {
+    // Count down from the value of iterations,
+    // changing the value of the element on the process
     element.textContent = iterations
     var tmp = setInterval(() => {
       iterations--
       element.textContent = iterations
+      // After enough iterations, the countdown stops
       if (iterations <= 0) {
         clearInterval(tmp)
         onresolve()
@@ -42,6 +49,7 @@ function countdown(iterations, element, callbackfn, interval = 1000) {
     }, interval)
   })
 
+  // Run the callback function once the promise resolves
   promise.then(callbackfn)
 }
 
